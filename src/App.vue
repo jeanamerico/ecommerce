5d552208d1850c281af2143c0a92a4067d0d58e5
Jean Americo <Jean.americo9898@gmail.com>
2020-11-24 12:45:08 -0300

    started project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="root-all">
+    <div id="nav-row">
+      <navrow/>
+    </div>
+  </div>
+</template>
+
+<script>
+import navrow from './components/navrow.vue'
+
+
+export default {
+  components: { navrow
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/navrow.vue
@@ -0,0 +1,70 @@
+<template>
+<div class="root-header">
+  <div class="logo-search">
+    <section class="logo">
+      <a class="" href="nav-button"><img src="../assets/logo-home.png" alt=""></a>
+    </section>
+    <section class="search">
+      <input type="text" placeholder="Pesquise itens aqui">
+    </section>
+  </div>
+  <div class="nav-row">
+    <ul>
+      <li class="nav-button"><a class="nav-button" href="">Homens</a></li>
+      <li class="nav-button"><a class="nav-button" href="">Mulheres</a></li>
+      <li class="nav-button"><a class="nav-button" href="">Esportes</a></li>
+      <li class="nav-button"><a class="nav-button" href="">Infantil</a></li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.logo-search{
+  justify-content: center;
+  display: flex;
+}
+
+.search{
+  display: flex;
+  margin: 10px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+.nav-button, .logotipo{
+  float: left;
+}
+
+li {
+  border-right: 1px solid #bbb;
+}
+
+img{
+  width: 100px;
+  height: 70px;
+}
+
+.nav-button {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 21px;
+  text-decoration: none;
+  
+}
+
+li a:hover {
+  background-color: #111;
+}
+</style>
