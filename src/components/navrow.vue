fb59210fbbd30473426e6a83fc9c031e755f8b32
Jean Americo <Jean.americo9898@gmail.com>
2020-11-30 19:01:09 -0300

    input search added

--- src/components/navrow.vue
+++ src/components/navrow.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="root-header">
     <v-bottom-navigation v-model="value" :background-color="color" dark shift>
+      <v-col class="inputpesq"
+        cols="12"
+        sm="6"
+        md="3">
+
+        <v-text-field
+          dense
+          filled
+          rounded
+          label="O que você está procurando?"
+        ></v-text-field>
+      </v-col>
+
       <v-btn>
         <span>Login</span>
         <v-icon>mdi-account-supervisor-circle-outline</v-icon>
@@ -21,6 +34,15 @@ export default {
 </script>
 
 <style>
+.inputpesq{
+  margin: 0 15% 0 0;
+}
+.v-bottom-navigation.v-item-group.theme--dark.v-bottom-navigation--shift{
+  height: 75px !important;
+}
+.v-text-field {
+  padding-top: 0 !important;
+}
 .v-item-group.v-bottom-navigation .v-btn.v-btn--active{
   color:#ffffff!important;
 }
