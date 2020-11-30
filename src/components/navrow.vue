5f23c707e35770394defb081310c955f575ccf1e
Jean Americo <Jean.americo9898@gmail.com>
2020-11-29 23:00:46 -0300

    added nav-bar material design

--- src/components/navrow.vue
+++ src/components/navrow.vue
@@ -1,132 +1,42 @@
 <template>
-<div class="root-header">
-  <div class="logo-search">
-    <section id="logotipo" class="cabecalho">
-      <a class="" href="nav-button"><img src="../assets/logo-home.png" alt=""></a>
-    </section>
-    <section id="pesquisa" class="cabecalho">
-      <form class="form-inline">
-        <input class="campopesquisa" type="text">
-        <label for="campopesquisa">O que você está procurando?</label>
-      </form>
-    </section>
-    <section id="botaodelogin" class="cabecalho">
-      <a class="botoes" href="">Login</a>
-      <a class="botoes" href="">Carrinho</a>
-    </section>
+  <div class="root-header">
+    <v-bottom-navigation
+    :value="value"
+    color="teal"
+    grow>
+
+      <v-btn>
+        <span>Recents</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </div>
-  <div class="nav-row">
-    <ul>
-      <li class="nav-button"><a class="nav-button" href="">Homens</a></li>
-      <li class="nav-button"><a class="nav-button" href="">Mulheres</a></li>
-      <li class="nav-button"><a class="nav-button" href="">Esportes</a></li>
-      <li class="nav-button"><a class="nav-button" href="">Infantil</a></li>
-      <li class="nav-button"><a class="nav-button" href="">Todas categorias</a></li>
-    </ul>
-  </div>
-</div>
 </template>
 
 <script>
-export default {};
+export default {
+    data: () => ({ value: 1 }),
+  }
+
 </script>
 
 <style>
-
-.root-header section{
-  display: table-cell;
-  vertical-align: middle;
+.v-item-group.v-bottom-navigation .v-btn.v-btn--active{
+  color:#009688!important;
 }
-
-#logotipo{
-  width: 10rem;
-  min-width: 10rem;
-  max-width: 10rem;
-  height: 5.25rem;
-  min-height: 1.25rem;
-  max-height: 5.25rem;
-}
-
-.cabecalho{
-  flex: auto; 
-}
-.logo-search{
-  justify-content: center;
-  display: flex;
-}
-#pesquisa{
-  margin:10px 0 0 120px;
-}
-
-.campopesquisa{
-  height: 35px;
-  border: none;
-  border-bottom: 1px solid black;
-}
-
-.form-inline {
-  position: relative;
-}
-
-.form-inline label,
-.form-inline input {
-  position: absolute;
-  width: 100%;
-  box-sizing: border-box;
-}
-
-.form-inline input {
-  top: 17px;
-}
-
-input:focus{
-  border:none;
-  outline: none;
-  border-bottom: 1px solid black;
-}
-
-#botaodelogin{
-  display: flex;
-  justify-content: center;
-  margin: 25px 0;
-}
-
-.botoes{
-  margin: 0 25px;
-  text-decoration: none;
-}
-
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
-
-.nav-button, .logotipo{
-  float: left;
-}
-
-li {
-  border-right: 1px solid #bbb;
-}
-
-img{
-  width: 100px;
-  height: 70px;
-}
-
-.nav-button {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 21px;
-  text-decoration: none;
-  
-}
-
-li a:hover {
-  background-color: #444;
+.v-btn:not(.v-btn--round).v-size--default{
+  height: 100% !important;
+      min-width: 64px;
+    padding: 0 16px;
 }
 </style>
